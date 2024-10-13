58a87ff009dd066d7f87dad6bfdf36acd43d89aa
JH Park <harryest@naver.com>
2024-10-13 14:15:36 +0900

    Feat: 회원 및 이벤트 관련 API 보안을 위한 Spring Security 설정 추가

--- src/main/java/Capstone/AutoScheduler/global/config/SecurityConfig.java
+++ src/main/java/Capstone/AutoScheduler/global/config/SecurityConfig.java
@@ -1,4 +1,37 @@
 package Capstone.AutoScheduler.global.config;
 
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                                // Member 관련 접근
+                                .requestMatchers("/members/sign-up", "/members/sign-in").permitAll()
+                                .requestMatchers("/members/**").permitAll()
+                                // Event 관련 접근
+                                .requestMatchers("/events/", "/events/{eventId}", "/events/member/{memberId}", "/events/member/{memberId}/event/{eventId}", "/events/member/{memberId}/date/{date}").permitAll()
+                                // 기타 관련 접근
+                                .requestMatchers("/", "/api-docs/**", "/api-docs/swagger-config/*", "/swagger-ui/*", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                //.addFilterBefore(new JwtAuthenticationFilter(), SecurityFilterChain.class)
+                .build();
+    }
 }
