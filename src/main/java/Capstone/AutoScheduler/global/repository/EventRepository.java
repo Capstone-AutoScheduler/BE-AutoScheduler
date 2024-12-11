e0d57258e72f523ec2426152549301a8c5c41e58
JH Park <harryest@naver.com>
2024-12-11 19:17:51 +0900

    Feat: 새로운 일정을 생성할때 겹치는 일정이 존재할 경우 예외처리 기능 추가

--- src/main/java/Capstone/AutoScheduler/global/repository/EventRepository.java
+++ src/main/java/Capstone/AutoScheduler/global/repository/EventRepository.java
@@ -3,8 +3,11 @@ package Capstone.AutoScheduler.global.repository;
 import Capstone.AutoScheduler.global.domain.entity.Event;
 import Capstone.AutoScheduler.global.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -12,4 +15,10 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     List<Event> findAllByMember(Member member);
     List<Event> findTop10ByMember_MemberIdOrderByCreatedAtDesc(Long memberId);
+
+    @Query("SELECT e FROM Event e WHERE e.member.id = :memberId " +
+            "AND ((:startDate <= e.endDate AND :endDate >= e.startDate))")
+    List<Event> findOverlappingEvents(@Param("memberId") Long memberId,
+                                      @Param("startDate") LocalDateTime startDate,
+                                      @Param("endDate") LocalDateTime endDate);
 }
