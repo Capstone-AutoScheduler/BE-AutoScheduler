6a9c82e56fc660e3000b1dab48d6030329a96f6b
JH Park <harryest@naver.com>
2024-12-13 02:43:10 +0900

    Refactor: GeneratorRequestDTO 수정

--- src/main/java/Capstone/AutoScheduler/global/web/dto/Generator/GeneratorRequestDTO.java
+++ src/main/java/Capstone/AutoScheduler/global/web/dto/Generator/GeneratorRequestDTO.java
@@ -18,13 +18,13 @@ public class GeneratorRequestDTO {
     public static class CreateGeneratorRequestDTO {
         private String generatorTitle;
         private String generatorDetail;
-        //private String frames;
-        //private String mapping;
-        // JSON 데이터를 리스트로 처리
         private List<Map<String, Object>> frames;
         private List<Map<String, Object>> mapping;
         private TypeSource sourceType;
         private String webUrl;
-        private Boolean loginRequired;
+
+        @Builder.Default
+        private Boolean loginRequired = Boolean.FALSE; // 기본값 false
     }
+
 }
